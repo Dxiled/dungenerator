d2e12889309d20ce1634cc2f553d6f400141b087
Bool0s <45517436+Bool0s@users.noreply.github.com>
2018-12-01 14:31:27 -0500

    Add files via upload
    ¯\_(ツ)_/¯

---
+++ API fetching.py
@@ -0,0 +1,31 @@
+import urllib.request
+import ast
+
+def readAPI(sublist):
+	response = urllib.request.urlopen("http://dnd5eapi.co/api/" + sublist)
+	html = response.readline()  # reads one line
+
+	data = ast.literal_eval(html.decode('utf-8'))
+
+	return data
+
+'''
+Get info of the race by their name
+returns the api's result as a dictionary
+'''
+def getRace(raceName):
+	raceName = raceName.lower()
+
+	raceIndex = {'dwarf':1,
+				 'elf':2,
+				 'halfling':3,
+				 'human':4,
+				 'dragonborn':5,
+				 'gnome':6,
+				 'half-elf':7,
+				 'half-orc':8,
+				 'teifling':9}
+
+	raceData = readAPI('races/' + str(raceIndex[raceName]))
+
+	return raceData
